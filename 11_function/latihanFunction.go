99b4351f3b920ae4c7fe13c4cede8ca78e8d1577
Evan alsadik <evanalsadik21@gmail.com>
2024-05-22 10:33:53 +0700

    updating file latihan function

--- 11_function/latihanFunction.go
+++ 11_function/latihanFunction.go
@@ -1,41 +1,43 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "bufio"
-  "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
-
 func main() {
-  scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-  fmt.Print("Masukan banyak bamboo:")
-  scanner.Scan()
-  jumlahBamboo, _ := strconv.Atoi(scanner.Text())
+	// fmt.Print("Masukan banyak bamboo: ")
+	scanner.Scan()
+	jumlahBamboo, _ := strconv.Atoi(scanner.Text())
 
-  jumlahSekat := make([]int, jumlahBamboo)
-  for i := 0; i < jumlahBamboo; i++{
-    fmt.Printf("Sekat bamboo ke-%d:", i+1)
-    scanner.Scan()
-    jumlah, _ := strconv.Atoi(scanner.Text())
-    jumlahSekat[i] = jumlah
-  }
-  fmt.Print("Berapa kali potong?")
-  scanner.Scan()
-  jumlahPotong, _ := strconv.Atoi(scanner.Text())
-  
-  cutOffTheBamboo(jumlahBamboo, jumlahSekat, jumlahPotong)
+	jumlahSekat := make([]int, jumlahBamboo)
+	for i := 0; i < jumlahBamboo; i++ {
+		// fmt.Printf("Sekat bamboo ke-%d: ", i+1)
+		scanner.Scan()
+		jumlah, _ := strconv.Atoi(scanner.Text())
+		jumlahSekat[i] = jumlah
+	}
+	// fmt.Print("Berapa kali potong? ")
+	scanner.Scan()
+	jumlahPotong, _ := strconv.Atoi(scanner.Text())
+
+	cutOffTheBamboo(jumlahSekat, jumlahPotong)
 }
 
-func cutOffTheBamboo(jumlahBamboo int, jumlahSekat []int, jumlahPotong int) {
-  for i := 1; i <= jumlahPotong; i++ {
-    fmt.Printf("Potongan ke-%d\n", i)
-    for j := 0; j <= jumlahBamboo-1; j++ {
-      jumlahSekat[j] -= 1 
-      fmt.Println(jumlahSekat[j])
-    }
-    fmt.Println()
-  }
+func cutOffTheBamboo(jumlahSekat []int, jumlahPotong int) {
+	for i := 1; i <= jumlahPotong; i++ {
+		fmt.Printf("Potongan ke- %d\n", i)
+		for j := 0; j < len(jumlahSekat); j++ {
+			if jumlahSekat[j] > 0 {
+				jumlahSekat[j] -= 1
+			}
+			fmt.Println(jumlahSekat[j])
+		}
+	}
+	fmt.Println()
 }
+
